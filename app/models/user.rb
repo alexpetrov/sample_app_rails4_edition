53090a2cad3b5f105f6e55acc8295c9e1cef0c43
Alexander Petrov <alexpetrov.rb@gmail.com>
2015-02-16 09:43:59 +0300

    Make user feed with SQL query with subquery

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,9 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }, allow_blank: true
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
   def follow(other_user)
